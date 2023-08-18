a9f80cf1e29ee33894bdf29ff3fc810c1fb24376
Bastiancst <121899892+Bastiancst@users.noreply.github.com>
2023-08-18 00:51:33 -0400

    init repositories for Martin

---
+++ Airline_DE/Wrappers/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace Airline_DE.Wrappers
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public T Result { get; set; }
+
+        public ApiResponse()
+        {
+
+        }
+
+        public ApiResponse(T data, string message = null)
+        {
+            Success = true;
+            Message = message;
+            Result = data;
+        }
+
+        public ApiResponse(string message)
+        {
+            Success = false;
+            Message = message;
+        }
+    }
+
+}
