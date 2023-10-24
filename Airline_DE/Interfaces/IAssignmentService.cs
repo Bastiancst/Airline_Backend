b311f270e5ca61ed2a38f7c4655a44b9d6200ead
martinmatiasz <martinmatiasz1978@gmail.com>
2023-10-24 13:49:33 -0300

    interfaces

---
+++ Airline_DE/Interfaces/IAssignmentService.cs
@@ -0,0 +1,16 @@
+﻿using Airline_DE.Models.Client.DTOs;
+using Airline_DE.Models.Client;
+using Airline_DE.Wrappers;
+using Airline_DE.Models.Assignment.DTOs;
+
+namespace Airline_DE.Interfaces
+{
+    public interface IAssignmentService
+    {
+        Task<ApiResponse<Guid>> CreateAsync(CreateAssignmentDTO request);
+        //Task<ApiResponse<Client>> GetByIdAsync(Guid clientId);
+        //Task<ApiResponse<IEnumerable<Client>>> GetAllAsync();
+        //Task<ApiResponse<Client>> UpdateAsync(UpdateClientDTO client, Guid id);
+        //Task<ApiResponse<Guid>> DeleteAsync(Guid client);
+    }
+}
---
+++ Airline_DE/Services/CRUDService/AssignmentService.cs
@@ -0,0 +1,53 @@
+﻿using Airline_DE.Interfaces;
+using Airline_DE.Interfaces.IRepository;
+using Airline_DE.Models.Assignment;
+using Airline_DE.Models.Assignment.DTOs;
+using Airline_DE.Models.Receiver;
+using Airline_DE.Wrappers;
+
+namespace Airline_DE.Services.CRUDService
+{
+    public class AssignmentService : IAssignmentService
+    {
+        private readonly IAssignmentRepository _assignment;
+        private readonly IReceiverRepository _receiver;
+
+        public AssignmentService(IAssignmentRepository assignment, IReceiverRepository receiver)
+        {
+            _assignment = assignment;
+            _receiver = receiver;
+        }
+
+        public async Task<ApiResponse<Guid>> CreateAsync(CreateAssignmentDTO request, Guid clientId)
+        {
+            var assignment = new Assignment
+            {
+                Id = Guid.NewGuid(),
+                ClientId = clientId,
+                ReceiverId = Guid.NewGuid(),
+                Height = request.Height,
+                Weight = request.Weight,
+                Wide = request.Wide,
+                Lenght = request.Lenght,
+                Origin = request.Origin,
+                Destination = request.Destination,
+                IsCopyDocumentEmail = request.IsCopyDocumentEmail,
+            };
+
+            var receiver = new Receiver
+            {
+                Id = assignment.ReceiverId,
+                Name = request.Receiver.Name,
+                LastName = request.Receiver.LastName,
+                Addres = request.Receiver.Addres,
+                PhoneNumber = request.Receiver.PhoneNumber,
+                Email = request.Receiver.Email,
+            };
+
+            await _assignment.CreateAsync(assignment);
+            await _receiver.CreateAsync(receiver);
+
+            return new ApiResponse<Guid>(clientId);
+        }
+    }
+}
